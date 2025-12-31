f2a2c88780ca22c612f1d4bea8db03453cdadf12
sonnelon <sonnelon4@gmail.com>
2025-12-31 11:51:08 +0300

    add: display logo and clear screen in repl

--- src/ui/REPL/repl_driver.c
+++ src/ui/REPL/repl_driver.c
@@ -1,21 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "repl_driver.h"
 
-static void clean_stdin();
+static inline void display_logo();
+static inline void clean_stdin();
+static inline void clear_screen();
 
 int run_repl(const char * file_path) {
+    clear_screen();
+    display_logo();
     while (1) {
         printf("(tbg) ");
         char s;
         scanf("%c", &s);
-        printf("%c\n", s);
         clean_stdin();
     }
 
     return 0;
 }
 
-void clean_stdin() {
+
+static inline void display_logo() {
+    puts("  _______ _             _____  ____   _____ ");
+    puts(" |__   __(_)           |  __ \\|  _ \\ / ____|");
+    puts("    | |   _ _ __  _   _| |  | | |_) | |  __ ");
+    puts("    | |  | | '_ \\| | | | |  | |  _ <| | |_ |");
+    puts("    | |  | | | | | |_| | |__| | |_) | |__| |");
+    puts("    |_|  |_|_| |_|\\__, |_____/|____/ \\_____|");
+    puts("                   __/ |                    ");
+    puts("                  |___/                     ");
+}
+
+static inline void clean_stdin() {
     int c;
     while ((c = getchar()) != '\n' && c != EOF);
+}
+
+static inline void clear_screen() {
+    #ifdef _WIN32
+        system("cls");
+    #else
+        system("clear");
+    #endif
 }
