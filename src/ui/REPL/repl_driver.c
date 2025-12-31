b5b38def65f1fd5ee88a25bfc1cc58cb309e623e
sonnelon <sonnelon4@gmail.com>
2025-12-31 12:26:21 +0300

    add: read commands from repl

--- src/ui/REPL/repl_driver.c
+++ src/ui/REPL/repl_driver.c
@@ -1,24 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "repl_driver.h"
 
+#define BUFFER_SIZE 1024
+
 static inline void display_logo();
-static inline void clean_stdin();
 static inline void clear_screen();
+static inline void display_start_info();
+static int read_commands(char * buffer, int buffer_len);
 
 int run_repl(const char * file_path) {
     clear_screen();
     display_logo();
+    display_start_info();
     while (1) {
         printf("(tbg) ");
-        char s;
-        scanf("%c", &s);
-        clean_stdin();
+        char buffer[BUFFER_SIZE];
+        read_commands(buffer, BUFFER_SIZE);
+        puts(buffer);
     }
 
     return 0;
 }
 
+static int read_commands(char * buffer, int buffer_len) {
+    int i;
+    for (i = 0; i < buffer_len - 1; i++) {
+        int ch = getchar();
+        if (ch == '\n' || ch == EOF) break;
+        buffer[i] = (char)ch;
+    }
+    buffer[i] = '\0';
+    return 0;
+}
 
 static inline void display_logo() {
     puts("  _______ _             _____  ____   _____ ");
@@ -31,9 +46,12 @@ static inline void display_logo() {
     puts("                  |___/                     ");
 }
 
-static inline void clean_stdin() {
-    int c;
-    while ((c = getchar()) != '\n' && c != EOF);
+static inline void display_start_info() {
+    puts("b [ADDR] --- Set a breakpoint.");
+    puts("r [OPTS] --- Start a program.");
+    puts("n        --- Proceed to the next step.");
+    puts("s        --- Enter the function.");
+    puts("h        --- Display help information.");
 }
 
 static inline void clear_screen() {
