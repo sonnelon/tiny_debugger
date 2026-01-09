755742d157b56d83d9ca2fba591c69f2f5865038
sonnelon <sonnelon4@gmail.com>
2026-01-09 15:44:00 +0300

    update: small refactoring

--- src/ui/REPL/repl_driver.c
+++ src/ui/REPL/repl_driver.c
@@ -1,28 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #include "repl_driver.h"
 #include "repl_dispatcher.h"
 
-#define MSGS_SZ 5
+static inline void display_logo ();
+static inline void display_start_info ();
+static dbg_err_t read_commands (char * buffer, int buffer_len);
 
-static inline void
-display_logo ();
-
-static inline void
-display_start_info ();
-
-static dbg_err_t 
-read_commands (char * buffer, int buffer_len);
-
-dbg_err_t
-repl_run (const char * file_path, struct dbg_t * dbg, volatile sig_atomic_t * want_exit) {
+dbg_err_t repl_run (const char * file_path, struct dbg_t * dbg, volatile sig_atomic_t * want_exit) 
+{
     system("clear");
     display_logo();
     display_start_info();
 
     int buff_sz = 1024;
-    while (1 && !(*want_exit)) {
+    while (1 && !(*want_exit)) 
+	{
         printf("tdb >> ");
         char buffer[buff_sz];
         read_commands(buffer, buff_sz);
@@ -32,11 +27,11 @@ repl_run (const char * file_path, struct dbg_t * dbg, volatile sig_atomic_t * wa
     return DBG_OK;
 }
 
-static dbg_err_t
-read_commands (char * buffer, int buffer_len) {
+static dbg_err_t read_commands (char * buffer, int buffer_len) 
+{
     int i;
-
-    for (i = 0; i < buffer_len - 1; i++) {
+    for (i = 0; i < buffer_len - 1; i++) 
+	{
         int ch = getchar();
         if (ch == '\n' || ch == EOF) break;
         buffer[i] = (char)ch;
@@ -46,8 +41,8 @@ read_commands (char * buffer, int buffer_len) {
     return DBG_OK;
 }
 
-static inline void 
-display_logo () {
+static inline void display_logo () 
+{
     puts("  _______ _             _____  ____   _____ ");
     puts(" |__   __(_)           |  __ \\|  _ \\ / ____|");
     puts("    | |   _ _ __  _   _| |  | | |_) | |  __ ");
@@ -58,15 +53,16 @@ display_logo () {
     puts("                  |___/                     ");
 }
 
-static inline void
-display_start_info () {
-    const char * msgs[MSGS_SZ] = {
-        "b [ADDR] --- Set a breakpoint.",
-        "r [OPTS] --- Start a program.", 
-        "n        --- Proceed to the next step.",
-        "s        --- Enter the function.",
-        "h        --- Display help information.",
+static inline void display_start_info () 
+{
+    const char * msgs[] = {
+        "b      [ADDR]    Set a breakpoint.",
+        "r/run  [OPTS]    Start a program.", 
+        "n/next           Proceed to the next step.",
+        "s/step           Enter the function.",
+        "h/help           Display help information.",
     };
-
-    for (int i = 0; i < MSGS_SZ; i++) puts(msgs[i]);
+	
+	const int msgs_len = sizeof(msgs)/sizeof(msgs[0]);
+    for (int i = 0; i < msgs_len; i++) puts(msgs[i]);
 }
